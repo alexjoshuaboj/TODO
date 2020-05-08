366a490c6cb12304fa0941f5ff9c0f7a4c6a4de9
alexjoshua <alejandrobojneag@gmail.com>
2020-05-08 13:12:17 +0200

    FINISH

--- js/main.js
+++ js/main.js
@@ -1,51 +1,150 @@
+
+// __________________________declaracione de variables_______________________ 
+// *************buttons*************
+var botonAdd = document.querySelector('#btnAdd');
+//donde se pintan los tasks
+var allreminde = document.querySelector('.alltasks');
+// id de los tasks para poder borrarlos
+var idTask = 7;
+//recogida faq de la parte de abajo del main
 var faq = $('.faq h2');
+//warrning rellene datos
+var msn = document.querySelector('#msn-try');
+//input serach value
+var inputSearch = document.querySelector('#search');
+//btn search all
+var btnSearch = document.querySelector('#searchBtnAll');
+//input choose
+var selectChoose = document.querySelector('#select-sectionAll');
+// variable array
 
+
+var btnSearch2 = $('#searchBtnAll');
+
+console.log(btnSearch2.text());
+
+
+var reminde = new Array();// array
+reminde = [
+    { taskReD: 'Estudiar', idColor: 'beige', idReD: 0 },
+    { taskReD: 'Comer', idColor: 'militar-green', idReD: 1 },
+    { taskReD: 'Beber RedBull', idColor: 'yellow', idReD: 2 },
+    { taskReD: 'Limpiar', idColor: 'militar-green', idReD: 3 },
+    { taskReD: 'Tatuarme', idColor: 'yellow', idReD: 4 },
+    { taskReD: 'Pasear la tortuga', idColor: 'militar-green', idReD: 5 },
+    { taskReD: 'Aprender CSS :(', idColor: 'beige', idReD: 6 },
+];
+
+
+// function faq acordeo (solo por practocar XD) 
 faq.on('click', function () {
-    //more easy
-    /* $(this).next().slideToggle(400); */
-
-    //relative easy
     $('.faq p').slideUp(400);
     $('.faq h2').removeClass('titulo');
-    /* $('.faq h2').css('background-color', 'tomato') */
-
     let contenido = $(this).next();
-
     if (contenido.css('display') === 'none') {
         contenido.slideDown(400);
-        /* $(this).css('background-color', 'lightgrey') */
         $(this).addClass('titulo');
     } else {
         contenido.slideUp(400);
     }
 })
 
-var textoArea = $('#texto-area');
+function addTasks(pTitTask, pCatTask) {
+    let newTask = {};
 
-console.log(textoArea.text());
+    newTask.taskReD = pTitTask
+    newTask.idColor = pCatTask
+    newTask.idReD = idTask;
+    idTask++
+    reminde.push(newTask);
+    printOneTask(newTask);
+
+}
+//boton que recoge los valores del submit y el texto para posteriormete guardarlos y enviarlos a addTqasks
+botonAdd.addEventListener('click', () => {
+    let title = document.querySelector('#title-add').value;
+    let cat = document.querySelector('#select-section').value;
+    let warning = document.querySelector('#add-reminde');
+    msn.innerText = "";
+    warning.style.border = "";
+    if (title != "" && cat != "") {
+        addTasks(title, cat);
+    } else {
+        msn.innerText = "Empty input"
+        warning.style.border = '1px solid red';
+    }
+})
+//print task 
+function printOneTask(pTask) {
+    //precario
+    switch (pTask.idColor) {
+        case "yellow":
+            pTask.idColor = "yellow";
+            break;
+        case "militar-green":
+            pTask.idColor = "militar-green";
+            break;
+        case "beige":
+            pTask.idColor = "beige";
+            break;
+    }
+
+    allreminde.innerHTML +=
+        `
+        <div class="task" id="${pTask.idReD}">
+            <p>${pTask.taskReD}</p>
+            <div id="${pTask.idColor}">
+            </div>
+            <button id="btn-remove">Delete</button>
+        </div>
+        `
 
 
-// declaracion del ARRAY guarda los datos
+}
+//print all tasks
+function printAllTasks(pArrayTask) {
+    for (task of pArrayTask) {
+        printOneTask(task);
+    }
+}
+printAllTasks(reminde);
 
-const JSON_ReD = new Array(
-    {
-        cat_ReD: 'Important',
-        tit_ReD: 'Estudiar',
-        txt_ReD: 'Estudiar los diferentes tipos de bucles de JAVASCRIPT',
-        data_Imp: 1,
-    },
-    {
-        cat_ReD: 'Daily',
-        tit_ReD: 'Comer',
-        txt_ReD: 'Preparar la comida para todo el dia',
-        data_Imp: 2,
-    },
-    {
-        cat_ReD: 'Weekly',
-        tit_ReD: 'Limpiar',
-        txt_ReD: 'Tienes que limpiar puto cerdo',
-        data_Imp: 3,
-    },
-)
+//find task tambien se puede hacer con un keyup pero me parece mas comodo asi
+inputSearch.addEventListener('input', () => {
 
-//3 categorias obligatorias 
+    let filterTasksArray = [];
+    allreminde.innerHTML = "";
+
+    for (let task of reminde) {
+        let txt = inputSearch.value.toLowerCase();
+        let choose = selectChoose.value;
+        if (task.taskReD.toLowerCase().indexOf(txt) != -1 && task.idColor.indexOf(choose) != -1) {
+            filterTasksArray.push(task);
+            allreminde.innerHTML +=
+                `
+            <div class="task" id="${task.idReD}">
+                <p>${task.taskReD}</p>
+                <div id="${task.idColor}">
+                </div>
+                <button id="btn-remove">Delete</button>
+            </div>
+            `
+        }
+    }
+})
+
+var divTask = document.querySelectorAll('.task');
+console.log(divTask);
+
+for (let div of divTask) {
+    div.addEventListener('click', deleteT);
+}
+
+//delete function
+
+
+function deleteT(event) {
+    console.log(event.target);
+    event.target.parentElement.remove();
+
+};
